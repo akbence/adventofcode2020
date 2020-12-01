5877ffc3a417bff4898da84ff3f33b8752621892
Peter Urbanovics <urbpeti@gmail.com>
2020-12-01 10:04:32 +0100

    Day1 2nd task

--- day1/Task.java
+++ day1/Task.java
@@ -6,9 +6,11 @@ import java.util.stream.Collectors;
 
 public class Task {
     public static void main(String[] args) throws IOException {
-        List<String> extractedLine = Files.readAllLines((Paths.get("./day1/input.txt")));
+        List<String> extractedLine = Files.readAllLines(Paths.get("./day1/input.txt"));
         Integer result = findEntriesAndMultiply(extractedLine);
-        System.out.println(result);
+        System.out.println("Task 1 solution: " + result);
+        Integer task2Result = findThreeEntriesAndMultiply(extractedLine);
+        System.out.println("Task 2 solution: " + task2Result);
     }
 
     private static Integer findEntriesAndMultiply(List<String> extractedLine) {
@@ -18,6 +20,21 @@ public class Task {
                 for (int k = i + 1; k < numbers.size(); k++) {
                     if (numbers.get(i) + numbers.get(j) + numbers.get(k) == 2020) {
                         System.out.println(numbers.get(i) + ", and " + numbers.get(j) + ", and " + numbers.get(k));
+                        return numbers.get(i) * numbers.get(j) * numbers.get(k);
+                    }
+                }
+            }
+        }
+        throw new RuntimeException("no result found");
+    }
+
+    private static Integer findThreeEntriesAndMultiply(List<String> extractedLine) {
+        List<Integer> numbers = extractedLine.stream().mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+        for (int i = 0; i < numbers.size(); i++) {
+            for (int j = i + 1; j < numbers.size(); j++) {
+                for (int k = j + 1; k < numbers.size(); k++) {
+                    if (numbers.get(i) + numbers.get(j) + numbers.get(k) == 2020) {
+                        System.out.println(numbers.get(i) + ", and " + numbers.get(j) + ", and" + numbers.get(k));
                         return numbers.get(i) * numbers.get(j) * numbers.get(k);
                     }
                 }
