8e408064f58a0943dfb7c8db92ad8c4e980c1e28
Ágocsi-Kiss Bence <akbence007@gmail.com>
2020-12-01 09:55:06 +0100

    day1 minor refakt

--- day1/Task.java
+++ day1/Task.java
@@ -1,13 +1,12 @@
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Task {
     public static void main(String[] args) throws IOException {
-        List<String> extractedLine = Files.readAllLines(Path.of(Paths.get("").toAbsolutePath().toString() + "/day1/input.txt"));
+        List<String> extractedLine = Files.readAllLines((Paths.get("./day1/input.txt")));
         Integer result = findEntriesAndMultiply(extractedLine);
         System.out.println(result);
     }
