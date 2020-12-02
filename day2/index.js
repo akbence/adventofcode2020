b811231dbc83d2197d56b822b90efb87bb146148
Ágocsi-Kiss Bence <akbence007@gmail.com>
2020-12-02 08:08:08 +0100

    day2, task2

--- day2/index.js
+++ day2/index.js
@@ -28,9 +28,22 @@ function validate(record) {
   return false;
 }
 
+function validate2(record) {
+  let firstOcc = record.password[record.min-1] == record.char
+  let secondOcc = record.password[record.max-1] == record.char
+  return firstOcc ^ secondOcc
+}
+
+
 function task1(input) {
   const validPasswodCount = input.map(parseLine).map(validate).reduce((a, c) => c ? a + 1 : a, 0)
   console.log(`Task1 solution: ${validPasswodCount}`)
 }
 
+function task2(input) {
+  const validPasswodCount = input.map(parseLine).map(validate2).reduce((a, c) => c ? a + 1 : a, 0)
+  console.log(`Task2 solution: ${validPasswodCount}`)
+}
+
 task1(input)
+task2(input)
