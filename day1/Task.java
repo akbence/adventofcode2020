c36b818caaf8a161083d7ba46ce62a6a1e019264
Ágocsi-Kiss Bence <akbence007@gmail.com>
2020-12-02 10:46:37 +0100

    instructions added for day1

--- day1/Task.java
+++ day1/Task.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 public class Task {
     public static void main(String[] args) throws IOException {
-        List<String> extractedLine = Files.readAllLines(Paths.get("./day1/input.txt"));
+        List<String> extractedLine = Files.readAllLines(Paths.get("./input.txt"));
         Integer result = findEntriesAndMultiply(extractedLine);
         System.out.println("Task 1 solution: " + result);
         Integer task2Result = findThreeEntriesAndMultiply(extractedLine);
@@ -34,7 +34,7 @@ public class Task {
             for (int j = i + 1; j < numbers.size(); j++) {
                 for (int k = j + 1; k < numbers.size(); k++) {
                     if (numbers.get(i) + numbers.get(j) + numbers.get(k) == 2020) {
-                        System.out.println(numbers.get(i) + ", and " + numbers.get(j) + ", and" + numbers.get(k));
+                        System.out.println(numbers.get(i) + ", and " + numbers.get(j) + ", and " + numbers.get(k));
                         return numbers.get(i) * numbers.get(j) * numbers.get(k);
                     }
                 }
---
+++ day1/instructions.md
@@ -0,0 +1,21 @@
+# Instructions
+
+## Prerequisites
+
+1.) Install Java JDK (version 9 or more)
+
+## Run
+
+1.) Open terminal at day1 directory
+
+2.) Run ```java Task.java```
+
+3.) Expected result should be:
+
+```
+447, and 611, and 962
+Task 1 solution: 262738554
+447, and 611, and 962
+Task 2 solution: 262738554
+
+```
