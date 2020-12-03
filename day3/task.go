2d2064c774db4a6e5fa1efe50e94986f8889c7f1
Ágocsi-Kiss Bence <akbence007@gmail.com>
2020-12-03 08:17:25 +0100

    day3 task1 solution

--- day3/task.go
+++ day3/task.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func readInput() []string {
@@ -27,5 +28,28 @@ func readInput() []string {
 }
 
 func main() {
-	fmt.Print(len(readInput()))
+	task1()
+}
+
+func countTree(slope slope, christmasMap []string) int {
+	treeCount := 0
+	currX := 0
+	for i := 0; i < len(christmasMap); i += slope.down {
+		if christmasMap[i][currX] == '#' {
+			treeCount++
+		}
+		currX = (currX + slope.right) % len(christmasMap[0])
+	}
+	return treeCount
+}
+
+type slope struct {
+	down  int
+	right int
+}
+
+func task1() {
+	var christmasMap = readInput()
+	slope := slope{1, 3}
+	fmt.Println("Task1 solution: " + strconv.Itoa(countTree(slope, christmasMap)))
 }
