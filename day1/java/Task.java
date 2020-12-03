bbf155435f24cf0463dfe1ad69b62c2ede1ba692
Ágocsi-Kiss Bence <akbence007@gmail.com>
2020-12-03 20:15:29 +0100

    day 1 refact + implementation in R

--- day1/java/Task.java
+++ day1/java/Task.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 public class Task {
     public static void main(String[] args) throws IOException {
-        List<String> extractedLine = Files.readAllLines(Paths.get("./input.txt"));
+        List<String> extractedLine = Files.readAllLines(Paths.get("../input.txt"));
         Integer result = findEntriesAndMultiply(extractedLine);
         System.out.println("Task 1 solution: " + result);
         Integer task2Result = findThreeEntriesAndMultiply(extractedLine);
@@ -17,11 +17,9 @@ public class Task {
         List<Integer> numbers = extractedLine.stream().mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
         for (int i = 0; i < numbers.size(); i++) {
             for (int j = i + 1; j < numbers.size(); j++) {
-                for (int k = i + 1; k < numbers.size(); k++) {
-                    if (numbers.get(i) + numbers.get(j) + numbers.get(k) == 2020) {
-                        System.out.println(numbers.get(i) + ", and " + numbers.get(j) + ", and " + numbers.get(k));
-                        return numbers.get(i) * numbers.get(j) * numbers.get(k);
-                    }
+                if (numbers.get(i) + numbers.get(j)== 2020) {
+                    System.out.println(numbers.get(i) + ", and " + numbers.get(j));
+                    return numbers.get(i) * numbers.get(j);
                 }
             }
         }
@@ -34,7 +32,7 @@ public class Task {
             for (int j = i + 1; j < numbers.size(); j++) {
                 for (int k = j + 1; k < numbers.size(); k++) {
                     if (numbers.get(i) + numbers.get(j) + numbers.get(k) == 2020) {
-                        System.out.println(numbers.get(i) + ", and " + numbers.get(j) + ", and " + numbers.get(k));
+                        System.out.println(numbers.get(i) + ", and " + numbers.get(j) + ", and" + numbers.get(k));
                         return numbers.get(i) * numbers.get(j) * numbers.get(k);
                     }
                 }
---
+++ day1/r/task.r
@@ -0,0 +1,39 @@
+   read_input <- function() {
+      fileName <- "../input.txt"
+      conn <- file(fileName,open="r")
+      lines <- readLines(conn, warn=FALSE)
+      close(conn)
+      numbers <- vector()
+      for (i in 1:length(lines)){
+         temp <- as.numeric(lines[i])
+         numbers <- c(numbers, temp)
+      }
+      numbers
+   }
+
+   task1 <- function(numbers) {
+      for (i in 1:length(numbers)) {
+         for (j in i:length(numbers)) {
+            if (sum(numbers[[i]], numbers[[j]]) == 2020) {
+               print( numbers[[i]] * numbers[[j]] )   
+            }
+         }
+      }
+   }
+
+   task2 <- function(numbers) {
+      for (i in 1:length(numbers)) {
+         for (j in i:length(numbers)) {
+            for (k in j:length(numbers)) {
+               if (sum(numbers[[i]], numbers[[j]], numbers[[k]]) == 2020) {
+                  print( numbers[[i]] * numbers[[j]]* numbers[[k]] )   
+               }
+            }
+         }
+      }
+   }
+   
+
+numbers <- read_input()
+task1(numbers)
+task2(numbers)
