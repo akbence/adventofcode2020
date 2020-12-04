1b88395b5358b625d9487664784e275c894bb71e
Ágocsi-Kiss Bence <akbence007@gmail.com>
2020-12-04 09:04:05 +0100

    fsharp tryhard

--- day4/fsharp/Program.fs
+++ day4/fsharp/Program.fs
@@ -1,8 +1,25 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.IO
+
+let filePath = "../input.txt"
+let readLines = seq {
+    use sr = new StreamReader (filePath)
+    while not sr.EndOfStream do
+        yield sr.ReadLine ()
+        
+}
+
+
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+    let lines = readLines
+    let splitted = lines |> Seq.head
+    printfn "%A" (splitted.Split ' ')
     0 // return an integer exit code
+
+
+
