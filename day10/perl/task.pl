722a5c6d3872654fcbe5a46c7e9a721f9161c431
Ágocsi-Kiss Bence <akbence007@gmail.com>
2020-12-10 18:23:32 +0100

    da10 task1

--- day10/perl/task.pl
+++ day10/perl/task.pl
@@ -18,6 +18,20 @@ sub fileRead()
 
 # Main
 @lines =  fileRead();
-for $line (@lines) {
-    print ($line);
+@sorted_lines = sort { $a <=> $b } @lines;
+$last_number=0;
+$one_jolt=0;
+$three_jolt=1;
+for $number (@sorted_lines) {
+    if($number-$last_number == 1){
+        $one_jolt++;
+    }
+
+    if($number-$last_number == 3){
+        $three_jolt++;
+    }
+    $last_number=$number;
 }
+
+$task1_solution=$one_jolt*$three_jolt;
+printf ("Task1 solution: %d",$task1_solution);
