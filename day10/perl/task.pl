3cb444b6054f12c5de663187dab62b7589b7a0b3
Ágocsi-Kiss Bence <akbence007@gmail.com>
2020-12-11 10:00:48 +0100

    first attempt, with cache, day10, task2

--- day10/perl/task.pl
+++ day10/perl/task.pl
@@ -1,13 +1,6 @@
 # !/usr/bin/perl 
-use Class::Struct;
 
-struct( Node => [
-    node_index  => '$',
-    connected_nodes => '@', 
-]);
-
-my @nodes=();
-my $counter=0;
+%cache = ();
 
 sub fileRead()
 {    
@@ -44,6 +37,10 @@ sub searchWay {
     my $curr_index = $_[1];
     my $last_index = $_[2];
     my (@sorted_lines) = @{$_[0]};
+
+    if(exists $cache{$curr_index}){
+        return $cache{$curr_index};
+    }
     my $result = 0;
     if ($curr_index == $last_index){
         return 1;
@@ -57,29 +54,8 @@ sub searchWay {
         if(($curr_index + 3 <= $last_index) && ($sorted_lines[$curr_index+3] - $sorted_lines[$curr_index] <=3 )){
             $result += searchWay(\@sorted_lines,$curr_index+3,$last_index);
         }
+    $cache{$curr_index} = $result;
     return $result;
-}
-
-
-sub dfsRecursive {
-    printf ("called");
-    my $u = $_[1];
-    my $d = $_[2];
-    my (@visited) = @{$_[0]};
-
-    if($u == $d){
-        $counter++;
-        return;
-    }
-
-    $visited[$u] = 1;
-    for $adj (@nodes[$u]->connected_nodes){
-        if($visited[$adj] == 0){
-            dfsRecursive(\@visited,$adj,$d);
-        }
-
-    }
-    $visited[$u] = 0;
 }
 
 sub task2(@lines){
@@ -90,33 +66,9 @@ sub task2(@lines){
     @sorted_lines = sort { $a <=> $b } @lines;
     push(@sorted_lines, $sorted_lines[-1]+3);
     $startIndex=0;
-
-    my $size= scalar @sorted_lines;
-    my $iter=0;
-    
-    for $number (@sorted_lines) {
-        my @connections = ();
-        if(($iter + 1 < $size) && ($sorted_lines[$iter+1] - $sorted_lines[$iter] <=3 )){
-            push(@connections, $iter+1);
-        }
-        if(($iter + 2 < $size )&& ($sorted_lines[$iter+2] - $sorted_lines[$iter] <=3 )){
-            push(@connections, $iter+2);
-        }
-        if(($iter + 3 < $size) && ($sorted_lines[$iter+3] - $sorted_lines[$iter] <=3 )){
-            push(@connections, $iter+3);
-        }
-        my $t = Node->new(node_index=>$iter,
-            connected_nodes=>\@connections);
-        push(@nodes, $t);
-        $iter++;
-    }
-
-    my @visited = (0) x scalar @nodes;
-    dfsRecursive( \@visited,$nodes[0]->node_index,$nodes[-1]->node_index);
-        
-        
-#    $solution= searchWay(\@sorted_lines,$startIndex, $size-1);
- #   printf("Task2 solution: %d", $solution);
+    $size= scalar @sorted_lines;
+    $solution= searchWay(\@sorted_lines,$startIndex, $size-1);
+    printf("Task2 solution: %d", $solution);
 }
 
 
